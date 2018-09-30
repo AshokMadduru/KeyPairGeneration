25d7d7ca5aec86397a7d16c81f12a095a18eaf98
Ashok Madduru <ashok.madduru@riktamtech.com>
2018-09-30 15:58:47 +0530

    EC keypair generation added

---
+++ app/src/main/java/com/shok/eckeypaigeneration/KeyUtils.java
@@ -0,0 +1,156 @@
+package com.shok.eckeypaigeneration;
+
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
+import android.util.Base64;
+import android.util.Log;
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.interfaces.ECPrivateKey;
+import java.security.interfaces.ECPublicKey;
+import java.security.spec.ECGenParameterSpec;
+
+/**
+ * Created by Ashok on 30/9/18.
+ */
+public class KeyUtils {
+  private final String KEY_ALIAS = "androidKey";
+  private final String ANDROID_KEY_STORE = "AndroidKeyStore";
+  private final String EC_CURVE_SPEC = "secp256r1";
+  private final String SIGNING_ALG = "SHA256withECDSA";
+
+  private KeyStore keyStore;
+  private ECPublicKey publicKey;
+
+  KeyUtils() {
+    generateKeyPairs();
+  }
+
+  /**
+   * Generates EC keypair
+   */
+  private void generateKeyPairs() {
+    KeyPairGenerator generator = null;
+    try {
+      generator = KeyPairGenerator.getInstance(KeyProperties.KEY_ALGORITHM_EC, ANDROID_KEY_STORE);
+      try {
+        KeyGenParameterSpec keyGenParameterSpec = new KeyGenParameterSpec.Builder(KEY_ALIAS,
+            KeyProperties.PURPOSE_SIGN).setAlgorithmParameterSpec(
+            new ECGenParameterSpec(EC_CURVE_SPEC)).build();
+        generator.initialize(keyGenParameterSpec);
+      } catch (InvalidAlgorithmParameterException e) {
+        e.printStackTrace();
+      }
+      generator.generateKeyPair();
+    } catch (NoSuchAlgorithmException | NoSuchProviderException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   *
+   * @param message message to be encoded
+   * @return Base64 encoded message with url safe
+   */
+  public String signMessage(String message) {
+    try {
+      Signature signature = Signature.getInstance(SIGNING_ALG);
+      getKeyStoreInstance();
+      try {
+        signature.initSign(getPrivateKey());
+        signature.update(message.getBytes());
+        byte[] sigBytes = signature.sign();
+        return Base64.encodeToString(sigBytes,
+            Base64.CRLF | Base64.URL_SAFE | Base64.NO_PADDING | Base64.NO_WRAP);
+      } catch (InvalidKeyException | SignatureException e) {
+        e.printStackTrace();
+      }
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    }
+    return "";
+  }
+
+  /**
+   *
+   * @return Base64 encoded x
+   */
+  public String getX() {
+    ECPublicKey publicKey = getPublicKey();
+    if (publicKey != null) {
+      return Base64.encodeToString(publicKey.getW().getAffineX().toByteArray(), Base64.CRLF);
+    }
+    return "";
+  }
+
+  /**
+   *
+   * @return Base64 encoded y
+   */
+  public String getY() {
+    ECPublicKey publicKey = getPublicKey();
+    if (publicKey != null) {
+      return Base64.encodeToString(publicKey.getW().getAffineY().toByteArray(), Base64.CRLF);
+    }
+    return "";
+  }
+
+  /**
+   * @return Private Key
+   */
+  private PrivateKey getPrivateKey() {
+    KeyStore keyStore = getKeyStoreInstance();
+    if (keyStore == null) {
+      return null;
+    }
+    try {
+      return (PrivateKey) keyStore.getKey(KEY_ALIAS, null);
+    } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  /**
+   * @return PublicKey
+   */
+  public ECPublicKey getPublicKey() {
+    KeyStore keyStore = getKeyStoreInstance();
+    if (publicKey == null && keyStore != null) {
+      try {
+        publicKey = (ECPublicKey) keyStore.getCertificate(KEY_ALIAS).getPublicKey();
+      } catch (KeyStoreException e) {
+        e.printStackTrace();
+      }
+    }
+    return publicKey;
+  }
+
+  /**
+   * Generates keystore instance
+   */
+  private KeyStore getKeyStoreInstance() {
+    if (keyStore == null) {
+      try {
+        keyStore = KeyStore.getInstance(ANDROID_KEY_STORE);
+        keyStore.load(null);
+      } catch (KeyStoreException | CertificateException | IOException | NoSuchAlgorithmException e) {
+        e.printStackTrace();
+      }
+    }
+    return keyStore;
+  }
+}
---
+++ app/src/main/java/com/shok/eckeypaigeneration/MainActivity.java
@@ -0,0 +1,34 @@
+package com.shok.eckeypaigeneration;
+
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.interfaces.ECPrivateKey;
+import java.security.interfaces.ECPublicKey;
+import java.security.spec.ECGenParameterSpec;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
+  @Override protected void onResume() {
+    super.onResume();
+
+    KeyUtils keyUtils = new KeyUtils();
+    String x = keyUtils.getX();
+    String y = keyUtils.getY();
+    Log.d("x:", x);
+    Log.d("y:", y);
+  }
+}
